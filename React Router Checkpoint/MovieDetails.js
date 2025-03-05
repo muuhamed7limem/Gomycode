60058d13aca399585a355161e4112b4fff6e8630
borutoboy <96014170+muuhamed7limem@users.noreply.github.com>
2025-03-05 20:50:47 +0100

    Update MovieDetails.js

--- React Router Checkpoint/MovieDetails.js
+++ React Router Checkpoint/MovieDetails.js
@@ -1,1 +1,34 @@
+// src/components/MovieDetails.js
+import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
+const MovieDetails = ({ movies }) => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const movie = movies.find((movie) => movie.id === parseInt(id));
+
+  if (!movie) {
+    return <div>Movie not found!</div>;
+  }
+
+  return (
+    <div className="movie-details">
+      <h1>{movie.title}</h1>
+      <p>{movie.description}</p>
+      <div className="trailer">
+        <iframe
+          width="560"
+          height="315"
+          src={movie.trailerLink}
+          title={movie.title}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <button onClick={() => navigate('/')}>Back to Home</button>
+    </div>
+  );
+};
+
+export default MovieDetails;
