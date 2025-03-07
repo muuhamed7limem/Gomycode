d23e9c49f8f9d80b9971914bc97e76bea479baa9
borutoboy <96014170+muuhamed7limem@users.noreply.github.com>
2025-03-07 18:03:44 +0100

    Add files via upload

---
+++ Checkpoint TypeScript Fundamentals/vehicle-app/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Car_1 = require("./classes/Car");
+// Create an instance of Car
+const myCar = new Car_1.Car('Toyota', 'Corolla', 2023);
+// Call the start method
+myCar.start();
---
+++ Checkpoint TypeScript Fundamentals/vehicle-app/src/index.ts
@@ -0,0 +1,7 @@
+import { Car } from './classes/Car';
+
+// Create an instance of Car
+const myCar = new Car('Toyota', 'Corolla', 2023);
+
+// Call the start method
+myCar.start();
